20148008349bdad8fd6226ac8c3daf41907a9552
AmyFiori <fiori_amanda@yahoo.com>
2018-05-21 15:57:57 -0400

    Introduced keyboard shortcuts to mindmap
    New shortcuts:
    
    Enter creates a new younger sibling for the selected label, if the
    selected label has a parent and the user isn’t editing a label (if they
    are, Enter just means they’re done editing).
    
    Tab creates a new child label for the selected label. If the selected
    label has children already, the new one appears at the end of the list.
    If the selected label’s children are hidden, they are displayed and the
    new one is added as normal.
    
    Delete deletes the selected label. For the time being, it also deletes
    that label’s children. It does not select a new label - when you delete
    a label, there is no selected label until you select a new one manually.
    
    Escape deselects the selected label.
    
    The arrow keys move through a tree. Left selects the selected label’s
    parent, if any - if the selected label is a root it does nothing. Right
    selects the selected label’s first child, if any - if the selected
    label is a leaf, it does nothing. As with tab, if the selected label
    has hidden children, they are shown first, and then the first one is
    selected as normal.
    
    Up and down cycle through the selected label’s siblings. If the
    selected label’s is its parent’s last child, down will cycle around to
    the first child, and if it’s its parent’s first child, up will cycle
    around to the last.

--- widgetSVG.js
+++ widgetSVG.js
@@ -204,25 +204,30 @@ class widgetSVG {
 
     // verify that the user doubleclicked on an EMPTY spot
     if (this.checkDrop(null, x, y) == null) {
-      // Create new object with no node associated
-      const newObj = {};
+      newObj = this.newObj();
       newObj.x = relX;
       newObj.y = relY;
-      newObj.nodeID = null;
-      newObj.id = this.count++;
-      newObj.name = "";
-      newObj.type = "";
-      newObj.parent = "null";
-      newObj.children = [];
-      newObj.details = [];
-
-      // Remember which node to edit
-      this.newNode = newObj.id;
-      this.newObject = newObj;
-
       this.roots.push(newObj);
       this.update();
     }
+  }
+
+  // Create new object with no node associated
+  newObj() {
+    const newObj = {};
+    newObj.nodeID = null;
+    newObj.id = this.count++;
+    newObj.name = "";
+    newObj.type = "";
+    newObj.parent = "null";
+    newObj.children = [];
+    newObj.details = [];
+
+    // Remember which node to edit
+    this.newNode = newObj.id;
+    this.newObject = newObj;
+
+    return newObj;
   }
 
   getObjFromID(nodeID) {
@@ -257,8 +262,183 @@ class widgetSVG {
   }
 
   keyPressed(evnt) {
-    alert ("Key pressed!");
+    if (evnt.target != this.editDOM) {
+      switch (evnt.which) {
+        case 9:
+          evnt.preventDefault();  // Don't jump around the page
+          this.tabKey();
+          break;
+        case 13:
+          this.enterKey();
+          break;
+        case 27:
+          this.escapeKey();
+          break;
+        case 37:
+          this.leftArrow();
+          break;
+        case 38:
+          evnt.preventDefault(); // Don't scroll
+          this.upDownArrow(-1); // On an up arrow, go to the previous sibling - SUBTRACT 1 from current index
+          break;
+        case 39:
+          this.rightArrow();
+          break;
+        case 40:
+          evnt.preventDefault(); // Don't scroll
+          this.upDownArrow(1); // on a down arrow, go to the next sibling -- ADD 1 to current index
+          break;
+        case 46:
+        case 8:
+          this.deleteKey();
+          break;
+      }
+    }
   }
+
+  // Creates a new blank node which is a child of the selected node
+  tabKey() {
+    if (this.selectedNode) {
+      const nodeID = this.selectedNode.getAttribute("idr").slice(5); // the IDR will be like groupxxx
+      const nodeObj = this.getObjFromID(nodeID); // Get the object representing this node
+
+      if (nodeObj._children && nodeObj._children.length > 0) { // If the object has children, but they are hidden, show them.
+        const button = this.selectedNode.children[1]; // Every node group has five children - a node rect, a toggle rect,
+        // a details rect, a details popup, and text - in that order. So the button is the child with index 1.
+        this.toggle(button);
+        // The children, if any, should now be visible, and the object should have a children array.
+      }
+
+      const child = this.newObj(); // Create a new blank label object...
+      child.parent = nodeID;
+      nodeObj.children.push(child); // make it a new child of the selected node...
+      this.update(); // and redraw the graphic.
+    }
+  }
+
+  // If NOT currently editing a node (in which case, hitting Enter just means "Done editing"),
+  // create a new younger sibling for the node.
+  enterKey() {
+    if (this.selectedNode) {
+      const nodeID = this.selectedNode.getAttribute("idr").slice(5); // the IDR will be like groupxxx
+      const nodeObj = this.getObjFromID(nodeID); // Get the object representing this node
+      const parentID = nodeObj.parent;
+      if (parentID != "null") { // IF the selected node has a parent, it can have siblings
+      const parent = this.getObjFromID(parentID);
+        const child = this.newObj();
+
+        const index = parent.children.indexOf(nodeObj) + 1; // Insert in the NEXT position, to come after the previous sibling
+        parent.children.splice(index, 0, child);
+        child.parent = parentID;
+        this.update();
+      }
+    }
+  }
+
+  // deselects the selected node
+  escapeKey() {
+    if (this.selectedNode) {
+      this.selectedNode.classList.remove("selected");
+      this.selectedNode = null;
+      this.update();
+    }
+  }
+
+  // Deletes the selected node and all of its children
+  deleteKey() {
+    if (this.selectedNode) {
+      const nodeID = this.selectedNode.getAttribute("idr").slice(5); // the IDR will be like groupxxx
+      const nodeObj = this.getObjFromID(nodeID); // Get the object representing this node
+      const parentID = nodeObj.parent;
+      if (parentID != "null") { // If the object has a parent, remove it from its parent's children array
+        const parentObj = this.getObjFromID(parentID);
+        const parentIndex = parentObj.children.indexOf(nodeObj);
+        if(parentIndex != -1) {
+          parentObj.children.splice(parentIndex, 1);
+        }
+      }
+      else { // If the object is a root, remove it from the roots array
+        const rootIndex = this.roots.indexOf(nodeObj);
+        if(rootIndex != -1) {
+          this.roots.splice(rootIndex, 1);
+        }
+      }
+      this.update();
+    }
+  }
+
+  // Selects the parent of the selected node
+  leftArrow() {
+    if (this.selectedNode) {
+      const nodeID = this.selectedNode.getAttribute("idr").slice(5); // the IDR will be like groupxxx
+      const nodeObj = this.getObjFromID(nodeID); // Get the object representing this node
+      const parentID = nodeObj.parent;
+      if (parentID != "null") { // If the object has a parent, select the parent
+        const parentGroup = app.domFunctions.getChildByIdr(this.SVG_DOM, `group${parentID}`);
+        this.selectedNode.classList.remove("selected");
+        this.selectedNode = parentGroup;
+        parentGroup.classList.add("selected");
+        this.update();
+      }
+    }
+  }
+
+  // Selects the first child of the selected node
+  rightArrow() {
+    if (this.selectedNode) {
+      const nodeID = this.selectedNode.getAttribute("idr").slice(5); // the IDR will be like groupxxx
+      const nodeObj = this.getObjFromID(nodeID); // Get the object representing this node
+        if (nodeObj._children && nodeObj._children.length > 0) { // If the object has children, but they are hidden, show them.
+          const button = this.selectedNode.children[1]; // Every node group has five children - a node rect, a toggle rect,
+          // a details rect, a details popup, and text - in that order. So the button is the child with index 1.
+          this.toggle(button);
+          // The children, if any, should now be visible.
+        }
+      if (nodeObj.children && nodeObj.children.length > 0) { // If the object has children, select the oldest child
+        const childObj = nodeObj.children[0];
+        const childID = childObj.id;
+        const childGroup = app.domFunctions.getChildByIdr(this.SVG_DOM, `group${childID}`);
+        this.selectedNode.classList.remove("selected");
+        this.selectedNode = childGroup;
+        childGroup.classList.add("selected");
+        this.update();
+      }
+    }
+  }
+
+  // Go to the previous sibling, if any. If this is the first sibling, cycle around to the last one.
+  upDownArrow(offset) {
+    if (this.selectedNode) {
+      const nodeID = this.selectedNode.getAttribute("idr").slice(5); // the IDR will be like groupxxx
+      const nodeObj = this.getObjFromID(nodeID); // Get the object representing this node
+      const parentID = nodeObj.parent;
+      if (parentID != "null") { // If the object has a parent, we can cycle through its siblings, if any
+        const parentObj = this.getObjFromID(parentID);
+        const parentIndex = parentObj.children.indexOf(nodeObj);
+        let newIndex = parentIndex + offset; // Add 1 to the index to go forward (down arrow). Subtract 1 to go back (up arrow)
+
+        // If we go too far backwards, the index will be -1. Cycle around to the last item
+        if (newIndex == -1) {
+          newIndex = parentObj.children.length - 1;
+        }
+
+        // If we go too far forward, the index will be equal to the array length. Cycle around to the first item.
+        if (newIndex == parentObj.children.length) {
+          newIndex = 0;
+        }
+
+        const siblingObj = parentObj.children[newIndex];
+        const siblingID = siblingObj.id;
+        const siblingGroup = app.domFunctions.getChildByIdr(this.SVG_DOM, `group${siblingID}`);
+
+        this.selectedNode.classList.remove("selected");
+        this.selectedNode = siblingGroup;
+        siblingGroup.classList.add("selected");
+      }
+      this.update();
+    }
+  }
+
 
   dropConnect(node, childObj) { // Creates a link between the node being dragged and the node it was dropped onto
     // Get object representing parent node (object representing child node was already found)
